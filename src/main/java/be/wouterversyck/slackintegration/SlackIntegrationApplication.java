d6177d302b53c9352660bcb0a00acc21d5acb120
Wouter Versyck <wouter.versyck@designisdead.com>
2018-12-19 16:31:25 +0100

    Changed package name

--- src/main/java/be/wouterversyck/slackintegration/SlackIntegrationApplication.java
+++ src/main/java/be/wouterversyck/slackintegration/SlackIntegrationApplication.java
@@ -1,14 +1,14 @@
-package com.poc.slackbotpoc;
+package be.wouterversyck.slackintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
 @SpringBootApplication(exclude = EmbeddedMongoAutoConfiguration.class)
-public class SlackBotPocApplication {
+public class SlackIntegrationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SlackBotPocApplication.class, args);
+        SpringApplication.run(SlackIntegrationApplication.class, args);
     }
 
 }
--- src/test/java/be/wouterversyck/slackintegration/SlackIntegrationApplicationTests.java
+++ src/test/java/be/wouterversyck/slackintegration/SlackIntegrationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.poc.slackbotpoc;
+package be.wouterversyck.slackintegration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SlackBotPocApplicationTests {
+public class SlackIntegrationApplicationTests {
 
     @Test
     public void contextLoads() {
